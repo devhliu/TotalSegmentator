4b39612da8f7b09c1bcfeedd4ec7a21361558869
Liu Hui <huiliu.liu@gmail.com>
2023-12-10 13:29:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 import sys
 from setuptools import setup, find_packages
 
-if sys.version_info < (3, 10):
-    # Specify the fixed version for Python < 3.10. Because using the latest
-    # requests would also install the latest urllib3 which does not work
-    # properly on python < 3.10.
-    requests_version = '==2.27.1'  #requires: urllib3>=1.21.1,<1.27 
-    # 2.27.1 somehow not available in dockerfile
-else:
-    requests_version = ''  # No fixed version for Python 3.10 and higher
-
+# requests is not used in this branch.
 
 setup(name='TotalSegmentator',
         version='2.0.5',
@@ -32,7 +24,6 @@ setup(name='TotalSegmentator',
             'xvfbwrapper',
             'fury',
             'nnunetv2>=2.1',
-            f'requests{requests_version}',
             'rt_utils',
             'dicom2nifti'
         ],
